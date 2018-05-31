295450b264fec5b449b8a6d14456597f524411dc
Yoshiroh Takanashi <takanashi@gclue.jp>
2018-05-31 16:27:17 +0900

    update

--- JetPack3.1/python3.6/scripts/install_pip3.sh
+++ JetPack3.1/python3.6/scripts/install_pip3.sh
@@ -6,6 +6,7 @@ apt-get install -y libjpeg-dev libxslt-dev libxml2-dev libffi-dev libcurl4-opens
 # packages
 apt-get install -y build-essential cmake libeigen3-dev libatlas-base-dev gfortran git wget libavformat-dev libavcodec-dev libswscale-dev libavresample-dev ffmpeg pkg-config unzip qtbase5-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev v4l-utils liblapacke-dev libopenblas-dev checkinstall libgdal-dev
 
+pip3 install --upgrade pip==9.0.3
 pip3 install --upgrade setuptools
 pip3 install --upgrade numpy
 #pip3 install --upgrade scipy
@@ -22,7 +23,7 @@ pip3 install --upgrade smbus2
 pip3 install --upgrade spidev
 pip3 install --upgrade absl-py
 pip3 install --upgrade sympy
-#pip3 install --upgrade pyyaml
+pip3 install --upgrade pyyaml
 #pip3 install --upgrade cython
 
 #pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
