6665fc6a12357c705719f40b44c127a7856c7079
Yoshiroh Takanashi <takanashi@gclue.jp>
2018-06-06 17:28:18 +0900

    update

--- JetPack3.2/python3.6/scripts/build_tensorflow_macros_patch.sh
+++ JetPack3.2/python3.6/scripts/build_tensorflow_macros_patch.sh
@@ -3,11 +3,14 @@
 #./tensorflow/core/kernels/gather_functor_gpu.cu.h(57): error: calling a __host__ function("__builtin_expect") from a __global__ function("tensorflow::GatherOpKernel< ::Eigen::half, int, (bool)1> ") is not allowed
 
 # sed
-# escape characters \'$.*/[]^
+# input escape characters \'$.*/[]^
 # 1. Write the regex between single quotes.
 # 2. \ -> \\
 # 3. ' -> '\''
 # 4. Put a backslash before $.*/[]^ and only those characters.
+# output escape characters &|
+# 1. Put a backslash before &| and only those characters.
+
 
 
 # before:
@@ -18,6 +21,5 @@
 TARGET=/compile/tensorflow/tensorflow/core/platform/macros.h
 
 BEFORE="#if TF_HAS_BUILTIN(__builtin_expect) || (defined(__GNUC__) && __GNUC__ >= 3)"
-AFTER="#if (!defined(__NVCC__)) && (TF_HAS_BUILTIN(__builtin_expect) || (defined(__GNUC__) && __GNUC__ >= 3))"
-sed -i 's/'"${BEFORE}"'/'"${AFTER}"'/g' $TARGET
-
+AFTER="#if (!defined(__NVCC__)) \&\& (TF_HAS_BUILTIN(__builtin_expect) \|\| (defined(__GNUC__) \&\& __GNUC__ >= 3))"
+sed -i 's/^'"${BEFORE}"'$/'"${AFTER}"'/g' $TARGET
